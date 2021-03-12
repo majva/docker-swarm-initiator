97de48ce48f1d8d5156c3358446c17fc66df0e54
majva <topcodermc@gmail.com>
2021-03-12 14:32:11 +0330

    write bash script function for configs

--- Full-CI-CD/0-Install-private-registry/install.sh
+++ Full-CI-CD/0-Install-private-registry/install.sh
@@ -4,44 +4,79 @@
     refrence: https://phoenixnap.com/kb/set-up-a-private-docker-registry
 '
 
-if [ "$EUID" -ne 0 ]
-  then echo -e "\033[31mRun this script on sudo permission ..."
-  exit
-fi
+check_root_access() {
+  if [ "$EUID" -ne 0 ]
+    then echo -e "\033[31mRun this script on sudo permission ..."
+    exit
+  fi
+}
 
-mkdir /root/registry && mkdir /root/registry/nginx && mkdir /root/registry/auth
-mkdir -p /root/nginx/conf.d && mkdir -p /root/nginx/ssl
-mv ./registry.conf /root/registry/nginx/conf.d
+create_registry_directories() {
+  if [ -d "/root/registry" ] 
+  then
+    echo -e "\033[32mRegistry directories exists." 
+  else
+    echo "Creating registry paths"
+    mkdir /root/registry
+    mkdir /root/registry/nginx
+    mkdir /root/registry/auth
+    mkdir -p /root/nginx/conf.d
+    mkdir -p /root/nginx/ssl
+  fi
+}
 
-mkdir /root/create-ssl && mkdir /root/create-ssl/keys
-mkdir /root/create-ssl/certs
+create_ssl_dir_and_file() {
+  if [ -d "/root/create-ssl" ]
+    then
+      echo -e "\033[32ssl directories exists."
+    else
+      echo "Creating ssls files ..."
+      mkdir /root/create-ssl 
+      mkdir /root/create-ssl/keys
+      mkdir /root/create-ssl/certs
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/create-ssl/keys/registry.key -out /root/create-ssl/certs/registry.crt
+      openssl req -x509 -nodes -days 365 -newkey rsa:2048 \ 
+      -keyout /root/create-ssl/keys/registry.key \
+      -out /root/create-ssl/certs/registry.crt
 
-openssl dhparam -out /root/create-ssl/certs/registry.pem 2048
-openssl dhparam -out /root/create-ssl/keys/registrykey.pem 2048
+      openssl dhparam -out /root/create-ssl/certs/registry.pem 2048
+      openssl dhparam -out /root/create-ssl/keys/registrykey.pem 2048
 
-cp /root/create-ssl/certs/registry.pem /root/nginx/ssl/
-cp /root/create-ssl/keys/registrykey.pem /root/nginx/ssl/
+      cp /root/create-ssl/certs/registry.pem /root/nginx/ssl/
+      cp /root/create-ssl/keys/registrykey.pem /root/nginx/ssl/
 
+    fi
+}
 
-echo "[+] Request a new password file named {registry.passwd} for your user"
+create_registry_passwrd() {
+  echo "[+] Request a new password file named {registry.passwd} for your user"
 
-htpasswd -Bc registry.passwd hacktor
+  htpasswd -Bc registry.passwd hacktor
 
-mkdir ./tmp
-touch ./tmp/rootCA
+  if [ ! -d "./tmp" ]
+    then
+      mkdir ./tmp
+      touch ./tmp/rootCA
+  fi
 
-openssl x509 -in rootCA.pem -inform PEM -out ./tmp/rootCA.crt
+  if [ ! -d "/etc/docker/certs.d/registry.hacktor.com" ]
+    then
+      mkdir -p /etc/docker/certs.d/registry.hacktor.com
+      mkdir -p /usr/share/ca-certificates/extra/
+  fi
 
-mkdir -p /etc/docker/certs.d/registry.hacktor.com/
+  openssl x509 -in rootCA.pem -inform PEM -out ./tmp/rootCA.crt
 
-cp ./tmp/rootCA.crt /etc/docker/certs.d/hacktor.com/
+  cp ./tmp/rootCA.crt /etc/docker/certs.d/hacktor.com/
+  cp ./tmp/rootCA.crt /usr/share/ca-certificates/extra/
 
-mkdir -p /usr/share/ca-certificates/extra/
-cp ./tmp/rootCA.crt /usr/share/ca-certificates/extra/
+  dpkg-reconfigure ca-certificates
+}
 
-dpkg-reconfigure ca-certificates
+check_root_access()
+create_registry_directories()
+create_ssl_dir_and_file()
+create_registry_passwrd()
 
 systemctl restart docker
 
