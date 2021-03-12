bbc100105d07c80f9f1125bf471aca2314fc35cb
majva <topcodermc@gmail.com>
2021-03-12 14:43:06 +0330

    fix some problem in functions

--- Full-CI-CD/0-Install-private-registry/install.sh
+++ Full-CI-CD/0-Install-private-registry/install.sh
@@ -34,18 +34,17 @@ create_ssl_dir_and_file() {
       mkdir /root/create-ssl 
       mkdir /root/create-ssl/keys
       mkdir /root/create-ssl/certs
-
-      openssl req -x509 -nodes -days 365 -newkey rsa:2048 \ 
-      -keyout /root/create-ssl/keys/registry.key \
-      -out /root/create-ssl/certs/registry.crt
-
-      openssl dhparam -out /root/create-ssl/certs/registry.pem 2048
-      openssl dhparam -out /root/create-ssl/keys/registrykey.pem 2048
-
-      cp /root/create-ssl/certs/registry.pem /root/nginx/ssl/
-      cp /root/create-ssl/keys/registrykey.pem /root/nginx/ssl/
-
     fi
+    
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \ 
+    -keyout /root/create-ssl/keys/registry.key \
+    -out /root/create-ssl/certs/registry.crt
+
+    openssl dhparam -out /root/create-ssl/certs/registry.pem 2048
+    openssl dhparam -out /root/create-ssl/keys/registrykey.pem 2048
+
+    cp /root/create-ssl/certs/registry.pem /root/nginx/ssl/
+    cp /root/create-ssl/keys/registrykey.pem /root/nginx/ssl/
 }
 
 create_registry_passwrd() {
