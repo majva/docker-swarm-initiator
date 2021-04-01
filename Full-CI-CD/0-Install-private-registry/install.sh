2b5d710e6ae618ffef572ac83920874d2de6904f
majva <topcodermc@gmail.com>
2021-04-01 19:30:57 +0430

    fix and change codes

--- Full-CI-CD/0-Install-private-registry/install.sh
+++ Full-CI-CD/0-Install-private-registry/install.sh
@@ -11,80 +11,15 @@ check_root_access() {
   fi
 }
 
-create_registry_directories() {
-  if [ -d "/root/registry" ] 
-  then
-    echo -e "\033[32mRegistry directories exists." 
-  else
-    echo "Creating registry paths"
-    mkdir /root/registry
-    mkdir /root/registry/nginx
-    mkdir /root/registry/auth
-    mkdir -p /root/registry/nginx/conf.d
-    mkdir -p /root/registry/nginx/ssl
-  fi
+initialize_server() {
+  mkdir -p /root/docker_data/certs
 
-  cp ./registry.conf /root/registry/nginx/conf.d/
-}
+  apt install openssl -y
 
-create_ssl_dir_and_file() {
-  if [ -d "/root/create-ssl" ]
-    then
-      echo -e "\033[32ssl directories exists."
-    else
-      echo "Creating ssls files ..."
-      mkdir /root/create-ssl 
-      mkdir /root/create-ssl/keys
-      mkdir /root/create-ssl/certs
-    fi
-    
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /root/create-ssl/keys/fullchain.key -out /root/create-ssl/certs/fullchain.crt
+  openssl req -newkey rsa:4096 -nodes -sha256 -keyout /root/docker_data/certs/domain.key -x509 -days 365 -out /docker_data/certs/domain.crt
 
-    openssl dhparam -out /root/create-ssl/certs/fullchain.pem 2048
-    openssl dhparam -out /root/create-ssl/keys/privkey.pem 2048
+  mkdir -p /root/docker_data/image  
 
-    cp /root/create-ssl/certs/fullchain.pem /root/registry/nginx/ssl/
-    cp /root/create-ssl/keys/privkey.pem /root/registry/nginx/ssl/
-}
-
-create_registry_passwrd() {
-  echo "[+] Request a new password file named {registry.passwd} for your user"
-  
-  apt install apache2-utils -y
-
-  htpasswd -Bc registry.passwd hacktor
-  mv ./registry.passwd /root/registry/auth/registry.passwd
-
-  if [ ! -d "./tmp" ]
-    then
-      mkdir ./tmp
-      touch ./tmp/rootCA.crt
-  fi
-
-  if [ ! -d "/etc/docker/certs.d/registry.hacktor.com" ]
-    then
-      mkdir -p /etc/docker/certs.d/registry.hacktor.com
-      mkdir -p /usr/share/ca-certificates/extra/
-  fi
-
-  openssl x509 -in rootCA.pem -inform PEM -out ./tmp/rootCA.crt
-
-  mkdir -p /etc/docker/certs.d/registry.hacktor.com/
-  mkdir -p /usr/share/ca-certificates/extra/
-  cp ./tmp/rootCA.crt /etc/docker/certs.d/registry.hacktor.com/
-  cp ./tmp/rootCA.crt /usr/share/ca-certificates/extra/
-  rm -rf ./tmp
-
-  dpkg-reconfigure ca-certificates
 }
 
 check_root_access
-create_registry_directories
-create_ssl_dir_and_file
-create_registry_passwrd
-
-systemctl restart docker
-
-echo "[+] Start building image ..."
-
-docker-compose up -d
