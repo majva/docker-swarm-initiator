0c4a6e6220adbf87b0628c98f5334c518796d5af
mohammad javad ghasempour <topcodermc@gmail.com>
2021-08-25 11:53:18 +0430

    new registry design

--- CI-CD/0-Install-private-registry/install.bash
+++ CI-CD/0-Install-private-registry/install.bash
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-: ' write refrences for more searching
-    refrence: https://phoenixnap.com/kb/set-up-a-private-docker-registry
-'
-
 check_root_access() {
   if [ "$EUID" -ne 0 ]
-    then echo -e "\033[31mRun this script on sudo permission ..."
-    exit
-  fi
+    then echo -e 'Run this script on sudo permission ...'
+    exist
+  fi 
 }
+
+check_root_access
