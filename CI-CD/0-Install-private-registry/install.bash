a9895d5d5cc34d3f1bdf95dffbfa5567157eb0ee
mohammad javad ghasempour <topcodermc@gmail.com>
2021-08-25 11:36:17 +0430

    new registry design

--- CI-CD/0-Install-private-registry/install.bash
+++ CI-CD/0-Install-private-registry/install.bash
@@ -14,36 +14,34 @@ source .bashrc
 # step 1 - create working directory
 
 mkdir -p /root/docker/registry
-set -e
-docker_registry_src=/root/docker/registry
 
-mkdir -p $docker_registry_src/nginx && mkdir $docker_registry_src/auth
-mkdir -p $docker_registry_src/nginx/conf.d 
-mkdir -p $docker_registry_src/nginx/ssl
-mkdir -p $docker_registry_src/nginx/certs.d
+mkdir -p $/root/docker/registry/nginx && mkdir $/root/docker/registry/auth
+mkdir -p $/root/docker/registry/nginx/conf.d 
+mkdir -p $/root/docker/registry/nginx/ssl
+mkdir -p $/root/docker/registry/nginx/certs.d
 
-cp registry.conf $docker_registry_src/nginx/conf.d
+cp registry.conf $/root/docker/registry/nginx/conf.d
 
 apt install openssl -y
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout $docker_registry_src/nginx/certs.d/domain.key -x509 -days 365 -out $docker_registry_src/nginx/certs.d/domain.crt
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout $/root/docker/registry/nginx/certs.d/domain.key -x509 -days 365 -out $/root/docker/registry/nginx/certs.d/domain.crt
 
-openssl x509 -in $docker_registry_src/nginx/ssl/fullchain.pem -inform PEM -out $docker_registry_src/nginx/certs.d/domain.crt
-openssl x509 -in $docker_registry_src/nginx/ssl/privkey.pem -inform PEM -out $docker_registry_src/nginx/certs.d/domain.key
+openssl x509 -in $/root/docker/registry/nginx/ssl/fullchain.pem -inform PEM -out $/root/docker/registry/nginx/certs.d/domain.crt
+openssl x509 -in $/root/docker/registry/nginx/ssl/privkey.pem -inform PEM -out $/root/docker/registry/nginx/certs.d/domain.key
 
-htpasswd -Bc $docker_registry_src/auth/registry.passwd example
+htpasswd -Bc $/root/docker/registry/auth/registry.passwd example
 
-mkdir -p $docker_registry_src/ca
+mkdir -p $/root/docker/registry/ca
 
-openssl x509 -in $docker_registry_src/ca/rootCA.pem -inform PEM -out $docker_registry_src/ca/rootCA.crt
+openssl x509 -in $/root/docker/registry/ca/rootCA.pem -inform PEM -out $/root/docker/registry/ca/rootCA.crt
 
 mkdir -p /etc/docker/certs.d
 
-cp $docker_registry_src/ca/rootCA.crt /etc/docker/certs.d/$REGISTRY_HOST
+cp $/root/docker/registry/ca/rootCA.crt /etc/docker/certs.d/$REGISTRY_HOST
 
 mkdir -p /usr/share/ca-certificates/extra/
 
-cp $docker_registry_src/ca/rootCA.crt /usr/share/ca-certificates/extra/
+cp $/root/docker/registry/ca/rootCA.crt /usr/share/ca-certificates/extra/
 
 dpkg-reconfigure ca-certificates
 
