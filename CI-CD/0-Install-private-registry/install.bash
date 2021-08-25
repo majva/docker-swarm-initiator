2fc25639cdbbcab2ee54af8d4d6fb8fc32fa59e0
mohammad javad ghasempour <topcodermc@gmail.com>
2021-08-25 12:06:15 +0430

    new registry design

--- CI-CD/0-Install-private-registry/install.bash
+++ CI-CD/0-Install-private-registry/install.bash
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+: ' write refrences for more searching
+    refrence: https://phoenixnap.com/kb/set-up-a-private-docker-registry
+'
+
 check_root_access() {
-    if [ "$EUID" -ne 0 ]
-    then echo -e 'Run this script on sudo permission ...'
-        exist
-    fi
+  if [ "$EUID" -ne 0 ]
+    then echo -e "\033[31mRun this script on sudo permission ..."
+    exit
+  fi
+}
+
+initialize_server() {
+  mkdir -p /root/docker_registry/certs
+
+  apt install openssl -y
+
+  openssl req -newkey rsa:4096 -nodes -sha256 -keyout /root/docker_data/certs/registry.example.com.key -x509 -days 365 -out /root/docker_data/certs/registry.example.com.crt
+
+  mkdir -p /root/docker_registry/data
+
 }
 
 check_root_access
+initialize_server
+
+docker-compose up -d
+
