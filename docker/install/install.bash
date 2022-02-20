853b4587ec1ea472544600b210297ee0b2cc4f2c
majva <topcodermc@gmail.com>
2022-02-20 15:44:58 +0330

    fix docker install bash script

--- docker/install/install.bash
+++ docker/install/install.bash
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "run this script on sudo permission"
-  exit
-fi
+func_check_permission() {
+  if [ "$EUID" -ne 0 ]
+    then echo "run this script on sudo permission"
+    exit
+  fi
+}
 
-# Uninstall old versions if exist
-sudo apt remove docker docker-engine docker.io containerd runc -y
+func_install_docker() {
 
-sudo apt autoremove -y
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-sudo apt update -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  sudo apt update && apt install docker-ce docker-ce-cli containerd.io -y
 
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo apt update
+  sudo chmod +x /usr/local/bin/docker-compose
 
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+}
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+func_check_permission
+func_install_docker
