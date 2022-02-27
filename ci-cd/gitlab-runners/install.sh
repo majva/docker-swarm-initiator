2558becc446a9229f80fff02e7abac6e7573ae89
majva <topcodermc@gmail.com>
2022-02-27 03:41:11 +0330

    fix problem

--- ci-cd/gitlab-runners/install.sh
+++ ci-cd/gitlab-runners/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source .bashrc
+# source .bashrc
 
 func_check_permission() {
   if [ "$EUID" -ne 0 ]
@@ -11,6 +11,7 @@ func_check_permission() {
 
 func_check_permission
 
-docker-compose -f gitlab-runner-compose.yml up -d --scale gitlab-runners=4
+docker-compose -f gitlab-runner-compose.yml up -d 
+#--scale gitlab-runners=4
 
 #docker stack deploy -c gitlab-runners.docker-compose.swarm.yml gitlab-runners
