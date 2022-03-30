987ba5bf966e21ff6a8057a5f5cde642057a1590
majva <topcodermc@gmail.com>
2022-03-30 19:47:14 +0430

    add sentry

---
+++ tool/logging/sentry/install.sh
@@ -0,0 +1,16 @@
+
+
+check_root_access() {
+  if [ "$EUID" -ne 0 ]
+    then echo -e "\033[31mRun this script on sudo permission ..."
+    exit
+  fi
+}
+
+check_root_access
+
+source .bashrc
+
+docker network create sentry
+
+docker-compose -f portainer.yml up -d
